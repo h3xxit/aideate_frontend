b7beb242ce308b7442de4352dc92babf55208458
RiaRosenauer <ria@rosenauer-layer.de>
2023-04-30 06:36:36 +0200

    working on solution

--- lib/SolutionView.dart
+++ lib/SolutionView.dart
@@ -18,6 +18,8 @@ class SolutionView extends StatefulWidget {
 class _SolutionViewState extends State<SolutionView> {
   List<Widget> childList = [];
 
+  boolean button_loading = false;
+
   @override
   void initState() {
     super.initState();
@@ -44,7 +46,8 @@ class _SolutionViewState extends State<SolutionView> {
               child: TextButton(
                 onPressed: () async {
 
-                  print("what is happending??");
+                  button_loading = true;
+                  
                   Solution? sol;
 
                   /*sol = Solution(
@@ -71,6 +74,7 @@ class _SolutionViewState extends State<SolutionView> {
                       childList = tmp;
                     });
                   };
+                  button_loading = false;
                   // Add your onPressed function here
                 },
                 child: Row(
@@ -79,8 +83,8 @@ class _SolutionViewState extends State<SolutionView> {
                     Icon(Icons.refresh, color: Colors.white), // Add reload icon
                     SizedBox(
                         width: 8), // Add some spacing between icon and text
-                    Text(
-                      'Reload/ Generate Solution from the information gathered so far',
+                    Text(button_loading?
+                      'Reload/ Generate Solution from the information gathered so far':'Generating the solution, please have some patience!',
                       style: TextStyle(
                         color: Colors.white,
                         fontSize: 16,
@@ -129,15 +133,15 @@ class SolutionText extends StatelessWidget {
 
   IconData getIcon() {
     switch (title) {
-      case "Problem":
+      case "Identified Problem":
         return Icons.error_outline;
-      case "Description":
+      case "Description how AI can help":
         return Icons.description;
-      case "Analysis":
+      case "Detailed Analysis":
         return Icons.analytics_outlined;
-      case "Risks":
+      case "Involved Risks":
         return Icons.warning_amber_outlined;
-      case "Data":
+      case "Required Data":
         return Icons.data_usage;
       default:
         return Icons.help_outline;
